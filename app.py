16a2c1ef340e9a1c073145034c638e87d4d68a42
AshleyRyu <ryujw0108@gmail.com>
2019-02-08 01:59:22 +0000

    flask db init, migrate, upgrade 후 index.html init

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 # flask 및 sqlalchemy 설정
 # app.config('SQLALCHEMY_DATABASE_URI')
+app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db_flask.sqlite3'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app = Flask(__name__)
 
 # sqlalchemy 및 migration 초기화
 db = SQLAlchemy(app)
@@ -14,7 +14,7 @@ migrate = Migrate(app, db)
 
 class Movie(db.Model):
     __tablename__ = 'movies'
-    id = db.Column(db.integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(80), unique=True)
     title_en = db.Column(db.String(80), unique=False)
     audience = db.Column(db.Integer, unique=False)
@@ -39,7 +39,8 @@ class Movie(db.Model):
 
 @app.route('/')
 def index():
-    return 'hello'
-    
+    movies = Movie.query.all()
+    return render_template('index.html', movies = movies)
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='8080', debug=True)
