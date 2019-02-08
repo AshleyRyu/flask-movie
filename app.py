110ad428fa67995c4ce33949128837df6ed7b21a
AshleyRyu <ryujw0108@gmail.com>
2019-02-08 01:23:17 +0000

    flask/sqlalchemy 초기 설정 sqlalchemy, migration 초기화 setting

--- app.py
+++ app.py
@@ -2,7 +2,16 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
+# flask 및 sqlalchemy 설정
+# app.config('SQLALCHEMY_DATABASE_URI')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db_flask.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app = Flask(__name__)
+
+# sqlalchemy 및 migration 초기화
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
 
 @app.route('/')
 def index():
